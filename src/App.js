228773c55d070606acea8278b6c7ec6e6491ac69
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-11 10:00:03 +0100

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
 import Greeting from './components/Greeting';
-import { Provider } from "react-redux";
-import store from './configureStore'
-
-
+import store from './configureStore';
 
 function App() {
   return (
     <Provider store={store()}>
-    <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<Greeting />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-    
-   
+
   );
 }
 
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,21 +1,20 @@
-
-   
-import React, {useEffect} from "react"
-import { useSelector,useDispatch } from "react-redux"
-import { fetchGreeting } from "../configureStore"
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../configureStore';
 
 export default function Greeting() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchGreeting())
-    
-  }, [dispatch])
-  const greeting = useSelector(state => state.greeting)
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+  const greeting = useSelector((state) => state.greeting);
 
   return (
-    <React.Fragment>
-    Greetings: {greeting}
-  </React.Fragment>
-  )
+    <>
+      Greetings:
+      {' '}
+      {greeting}
+    </>
+  );
 }
